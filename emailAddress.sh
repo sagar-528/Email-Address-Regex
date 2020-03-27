3ed1da7b8456fadbc37cd825ac7505676838e8a8
sagar-528 <gupta.sagar528@gmail.com>
2020-03-28 00:40:36 +0530

    ADDED: Complete email validate

--- emailAddress.sh
+++ emailAddress.sh
@@ -7,7 +7,7 @@ echo "Validate Email address with a regex"
 echo " "
 read -p "Enter Email address: " email
 
-emailValidate="^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}$"
+emailValidate="^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})$"
 if [[ $email =~ $emailValidate ]]
 then
 	echo Yes
