787da433ebc1e94f78e3f72528bba9039cc7a56e
sagar-528 <gupta.sagar528@gmail.com>
2020-03-28 00:05:54 +0530

    ADDED: Name for email

---
+++ emailAddress.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x
+
+#->Author: Sagar Gupta 
+#->Date: Mar 2020 
+
+echo "Validate Email address with a regex"
+echo " "
+read -p "Enter Email address: " email
+
+emailValidate="[0-9a-zA-Z]$"
+if [[ $email =~ $emailValidate ]]
+then
+	echo Yes
+else
+	echo No
+fi
